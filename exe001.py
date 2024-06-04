071943e1dad4992959b2d9e16dd8afe721bfb02f
Euzebio Borges <olivertinoborges.euzebio@gmail.com>
2024-06-04 20:26:08 -0300

    Arquivo Python de exercícios 001

---
+++ exe001.py
@@ -0,0 +1,11 @@
+### Criando um pequeno projeto de exercícios 
+
+print(==== DESAFIO 01 ====)
+
+nome = input('Qual é o seu nome? ')
+print('Olá !', nome, 'É um prazer conhecer você!')
+dia = input('Qual o dia de seu nascimento? ')
+mes= input('Qual o mês de seu nascimento? ')
+ano = input('Qual o ano de seu nascimento? ')
+print('Você nasceu em', dia, mes, ano, ', Correto !')
+
